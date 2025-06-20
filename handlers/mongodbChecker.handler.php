9e6414f0486a4f755e2306f07376f9f78dd542ab
Carlo Dulutan <202310776@fit.edu.ph>
2025-06-19 22:58:30 -0700

    feat(envSetter.util.php) added this

--- handlers/mongodbChecker.handler.php
+++ handlers/mongodbChecker.handler.php
@@ -1,11 +1,20 @@
 <?php
+require_once __DIR__ . '/../util/envSetter.util.php';
+
+$host = getenv('MONGO_HOST');
+$port = getenv('MONGO_PORT');
+$dbname = getenv('MONGO_DB');
+$username = getenv('MONGO_ROOT_USER');
+$password = getenv('MONGO_ROOT_PASSWORD');
+
+$uri = "mongodb://$username:$password@$host:$port";
+
 try {
-    $mongo = new MongoDB\Driver\Manager("mongodb://host.docker.internal:23567");
-
-    $command = new MongoDB\Driver\Command(["ping" => 1]);
-    $mongo->executeCommand("admin", $command);
-
-    echo "✅ Connected to MongoDB successfully.  <br>";
+    $mongo = new MongoDB\Driver\Manager($uri);
+    // Attempt a ping to test the connection
+    $command = new MongoDB\Driver\Command(['ping' => 1]);
+    $mongo->executeCommand($dbname, $command);
+    echo "✅ Connected to MongoDB successfully.";
 } catch (MongoDB\Driver\Exception\Exception $e) {
-    echo "❌ MongoDB connection failed: " . $e->getMessage() . "  <br>";
+    echo "❌ MongoDB connection failed: " . $e->getMessage();
 }
--- handlers/postgreChecker.handler.php
+++ handlers/postgreChecker.handler.php
@@ -1,19 +1,17 @@
 <?php
+require_once __DIR__ . '/../util/envSetter.util.php';
 
-$host = "host.docker.internal"; 
-$port = "5555";
-$username = "user";
-$password = "password";
-$dbname = "mydatabase";
+$host = getenv('POSTGRES_HOST');
+$port = getenv('POSTGRES_PORT');
+$dbname = getenv('POSTGRES_DB');
+$user = getenv('POSTGRES_USER');
+$password = getenv('POSTGRES_PASSWORD');
 
-$conn_string = "host=$host port=$port dbname=$dbname user=$username password=$password";
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
-$dbconn = pg_connect($conn_string);
-
-if (!$dbconn) {
-    echo "❌ Connection Failed: ", pg_last_error() . "  <br>";
-    exit();
+if (!$conn) {
+    echo "❌ Connection Failed: " . pg_last_error();
 } else {
-    echo "✔️ PostgreSQL Connection  <br>";
-    pg_close($dbconn);
+    echo "✅ PostgreSQL Connection";
+    pg_close($conn);
 }
